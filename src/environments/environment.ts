34c2afefe532a7fe456bb859805e6b3f77501ad7
bhavitagadara <168505737+bhavitagadara@users.noreply.github.com>
2025-05-08 18:48:23 +0530

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:9000', // Add your backend URL here
+    stripePublishableKey: 'pk_test_51QzFVmBo0wLbWohr0HdgddEDxVd6xQwyMCytkuKYScu7gkAZPXv9bObfddmVmkSVZq0MDt8vVcvDbPkLu1NI6dBN00A3lOS8m3'
+  };
+  
