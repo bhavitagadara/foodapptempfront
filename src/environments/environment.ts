39fe4e0f4891d9881ffe2020d669c5cb39ccb6fa
bhavitagadara <bhavita.gadara03@gmail.com>
2025-05-12 15:49:51 +0530

    Updated angular.json budget limits

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:9000', // Add your backend URL here
+    // apiUrl: 'http://localhost:9000', // Add your backend URL here
+    apiUrl: 'https://foodapptempbackend.onrender.com', // Add your backend URL here
     stripePublishableKey: 'pk_test_51QzFVmBo0wLbWohr0HdgddEDxVd6xQwyMCytkuKYScu7gkAZPXv9bObfddmVmkSVZq0MDt8vVcvDbPkLu1NI6dBN00A3lOS8m3'
   };
   
